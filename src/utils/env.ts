b75aa7d5ed23788afdbd03b78d14d58cef97142a
Karl J. Overå <k@rl.run>
2023-09-06 12:03:16 +0200

    fix unleash auth

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -27,6 +27,9 @@ export const serverEnvSchema = z.object({
     TOKEN_X_WELL_KNOWN_URL: z.string(),
     TOKEN_X_PRIVATE_JWK: z.string(),
     TOKEN_X_CLIENT_ID: z.string(),
+    // for unleash
+    UNLEASH_SERVER_API_URL: z.string().optional(),
+    UNLEASH_SERVER_API_TOKEN: z.string().optional(),
 })
 
 /**
@@ -57,6 +60,9 @@ const getRawServerConfig = (): Partial<unknown> =>
         TOKEN_X_WELL_KNOWN_URL: process.env.TOKEN_X_WELL_KNOWN_URL,
         IDPORTEN_CLIENT_ID: process.env.IDPORTEN_CLIENT_ID,
         IDPORTEN_WELL_KNOWN_URL: process.env.IDPORTEN_WELL_KNOWN_URL,
+        // for unleash
+        UNLEASH_SERVER_API_URL: process.env.UNLEASH_SERVER_API_URL,
+        UNLEASH_SERVER_API_TOKEN: process.env.UNLEASH_SERVER_API_TOKEN,
     }) satisfies Record<keyof ServerEnv, string | undefined>
 
 /**
