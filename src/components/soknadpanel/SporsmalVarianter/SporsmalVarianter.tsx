6a762f2bbdac0b538beeabfbc399ea8c36da2a72
Natalie Uranes <natalie.uranes@nav.no>
2024-01-22 17:37:51 +0100

    ignore sporsmal svartype bekreftelsespunkter

--- src/components/soknadpanel/SporsmalVarianter/SporsmalVarianter.tsx
+++ src/components/soknadpanel/SporsmalVarianter/SporsmalVarianter.tsx
@@ -29,6 +29,9 @@ export enum PossibleSvarEnum {
 
 export function SporsmalVarianter({ sporsmal }: SporsmalVarianterProps): ReactElement | null {
     switch (sporsmal.svartype) {
+        case SoknadSporsmalSvartypeEnum.Bekreftelsespunkter:
+            return null
+
         case SoknadSporsmalSvartypeEnum.CheckboxPanel:
         case SoknadSporsmalSvartypeEnum.Checkbox:
             return <Checkbox sporsmal={sporsmal} />
